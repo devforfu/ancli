f46907321b17fecf1d978a0e7998bf6ddb0d2143
devforfu <developer.z@outlook.com>
2019-06-29 20:25:57 +0200

    More signatures tests

--- tests/test_cli_created.py
+++ tests/test_cli_created.py
@@ -1,15 +1,6 @@
 from ancli import make_cli
 
 
-def some_required(a: int, b: bool = True): pass
-
-
-def none_required(x=1, y=2, z=3): pass
-
-
-def no_args(): pass
-
-
 def test_function_signature_with_all_required_params():
     def all_required(integer: int, floating: float, string: str): pass
 
@@ -19,3 +10,32 @@ def test_function_signature_with_all_required_params():
     assert spec[0] == {'name': 'integer', 'type': int, 'required': True}
     assert spec[1] == {'name': 'floating', 'type': float, 'required': True}
     assert spec[2] == {'name': 'string', 'type': str, 'required': True}
+
+
+def test_function_signature_with_default_params():
+    def all_optional(x: int = 1, y: int = 2, z: int = 3): pass
+
+    spec = make_cli(all_optional, spec_only=True)
+
+    assert len(spec) == 3
+    assert spec[0] == {'name': 'x', 'type': int, 'default': 1}
+    assert spec[1] == {'name': 'y', 'type': int, 'default': 2}
+    assert spec[2] == {'name': 'z', 'type': int, 'default': 3}
+
+
+def test_function_signature_with_required_and_default_params():
+    def some_optional(first: int, second: bool = False): pass
+
+    spec = make_cli(some_optional, spec_only=True)
+
+    assert len(spec) == 2
+    assert spec[0] == {'name': 'first', 'type': int, 'required': True}
+    assert spec[1] == {'name': 'second', 'type': bool, 'default': False}
+
+
+def test_function_signature_without_args():
+    def no_args(): pass
+
+    spec = make_cli(no_args, spec_only=True)
+
+    assert not spec
