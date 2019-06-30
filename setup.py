91ff58f81ddd97be2e449820e39491826a2c4d6b
devforfu <developer.z@outlook.com>
2019-06-30 19:52:45 +0200

    Better README

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version=__version__,
     maintainer='devforfu',
     description='Small utility to convert functions into CLI',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     url='https://github.com/devforfu/ancli',
     download_url='https://github.com/devforfu/ancli/archive/v0.1.1.tar.gz',
     install_requires=[],
